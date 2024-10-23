75c7bb7c5e2ba81ce0d29d76041b71b1445f10fb
Rajwol Khadka <rajwolkhadka4@gmail.com>
2024-10-23 11:39:39 -0400

    Update InteractiveRank.sql

--- InteractiveRank.sql
+++ InteractiveRank.sql
@@ -2,13 +2,13 @@
 DROP TABLE IS EXISTS #SalesMetrics
 CREATE TABLE #SalesMetrics (
     Sales_Agent NVARCHAR(100),
-	Manager NVARCHAR(100),
+    Manager NVARCHAR(100),
     Close_Value BIGINT,
     Wins INT,
     Losses INT,
     Average_Deal FLOAT,
     High_Value_Closes INT,
-	Quick_Closes INT,
+    Quick_Closes INT,
     Winrate DECIMAL(10,2)
 );
 
@@ -16,7 +16,7 @@ CREATE TABLE #SalesMetrics (
 INSERT INTO #SalesMetrics (Sales_Agent, Manager, Close_Value, Wins, Losses, Average_Deal, High_Value_Closes, Quick_Closes, Winrate)
 SELECT 
     st.Sales_Agent,
-	st.Manager,
+    st.Manager,
     SUM(sp.close_value) AS Close_Value,
     SUM(CASE WHEN sp.deal_stage = 'won' THEN 1 ELSE 0 END) AS Wins,
     SUM(CASE WHEN sp.deal_stage = 'lost' THEN 1 ELSE 0 END) AS Losses,
@@ -30,19 +30,20 @@ LEFT JOIN salesteams st ON sp.sales_agent = st.sales_agent
 GROUP BY st.sales_agent, st.manager;
 
 --Step 3: Rank Sales Agents based on a specific performance metric (Steps 1 & 2 Can be Deleted)
+
 DECLARE @metric NVARCHAR(50);
 SET @metric = 'Quick_Closes';  -- Change this to 'losses', 'winrate', 'AVGdeal', etc.
 SELECT 
     *,
     DENSE_RANK() OVER (ORDER BY 
         CASE 
-			WHEN @metric = 'Close_value' then Close_value
+	    WHEN @metric = 'Close_value' then Close_value
             WHEN @metric = 'Wins' THEN Wins
             WHEN @metric = 'Losses' THEN Losses
             WHEN @metric = 'Winrate' THEN Winrate
             WHEN @metric = 'Average_deal' THEN Average_Deal
-			WHEN @metric = 'High_Value_Closes' then High_Value_Closes
-			WHEN @metric = 'Quick_closes' then Quick_Closes
+	    WHEN @metric = 'High_Value_Closes' then High_Value_Closes
+	    WHEN @metric = 'Quick_closes' then Quick_Closes
             ELSE Close_Value  -- Default to closevalue if no metric is matched
         END DESC
     ) AS Overall_Rank,
@@ -51,12 +52,12 @@ SELECT
         ORDER BY 
             CASE 
                 WHEN @metric = 'Close_value' then Close_value
-				WHEN @metric = 'Wins' THEN Wins
+		WHEN @metric = 'Wins' THEN Wins
                 WHEN @metric = 'Losses' THEN Losses  
                 WHEN @metric = 'Winrate' THEN Winrate
                 WHEN @metric = 'Average_Deal' THEN Average_Deal
-				WHEN @metric = 'High_Value_Closes' then High_Value_Closes
-				WHEN @metric = 'Quick_closes' then Quick_Closes
+		WHEN @metric = 'High_Value_Closes' then High_Value_Closes
+		WHEN @metric = 'Quick_closes' then Quick_Closes
                 ELSE Close_Value  -- Default to closevalue
             END DESC
     ) AS team_rank
